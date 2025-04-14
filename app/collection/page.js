76de96028fbe300e14946cd5cf2e773876403d0b
Josephine <bonsujo@sheridancollege.ca>
2025-04-14 12:29:12 -0400

    Home page setup with css

---
+++ app/collection/page.js
@@ -0,0 +1,18 @@
+import { getAllInstruments } from "@/lib/api";
+import Link from "next/link";
+
+export default async function CollectionPage() {
+  const instruments = await getAllInstruments();
+
+  return (
+    <div>
+      <h1>Instruments</h1>
+      {instruments.map(instr => (
+        <div key={instr.id} style={{ marginBottom: '10px', border: '1px solid gray', padding: '10px' }}>
+          <p><strong>{instr.id}</strong>: {instr.instrument_name}</p>
+          <Link href={`/collection/${instr.id}`}>More</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
